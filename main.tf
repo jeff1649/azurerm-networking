dcd8c091b4bcff6dcb9a584821bb5a6ce098f6fe
jeff1649 <jeff1649@yahoo.com>
2026-09-08 10:22:38 -0500

    updated files

--- main.tf
+++ main.tf
@@ -1,36 +1,10 @@
 module "network" {
   source = "./modules/vnet"
 
-  vnet_name           = "vnet-tf-test"
-  location            = "centralus"
-  resource_group_name = "rg-terraform-test-poc"
-
-  address_space = [
-    "10.10.0.0/16"
-  ]
-
-  subnets = {
-    apps = {
-      name = "snet-apps"
-
-      address_prefixes = [
-        "10.10.1.0/24"
-      ]
-    }
-
-    private_endpoints = {
-      name = "snet-private-endpoints"
-
-      address_prefixes = [
-        "10.10.2.0/24"
-      ]
-
-      private_endpoint_network_policies = "Disabled"
-    }
-  }
-
-  tags = {
-    environment = "test"
-    managed_by  = "terraform"
-  }
+  vnet_name           = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+  subnets             = var.subnets
+  tags                = var.tags
 }

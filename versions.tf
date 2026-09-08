0084adfaa5d676933c77206967c288ac4b2b55ee
jeff1649 <jeff1649@yahoo.com>
2026-09-08 08:46:00 -0500

    Updated Terraform backend and plan workflow

--- versions.tf
+++ versions.tf
@@ -8,13 +8,13 @@ terraform {
     }
   }
 
-backend "azurerm" {
-  resource_group_name  = "rg-terraform-state"
-  storage_account_name = "stterraformstatepoc001"
-  container_name       = "state"
-  key                  = "azurerm-networking.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stterraformstatepoc001"
+    container_name       = "state"
+    key                  = "azurerm-networking.tfstate"
 
-  use_oidc       = true
-  use_azuread_auth = true
-    }
+    use_oidc         = true
+    use_azuread_auth = true
+  }
 }
